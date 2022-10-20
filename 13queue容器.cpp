42ad7aba031dc76214810465f93426d5788eb2fd
UE作品 <112238303+NNhuayoushu@users.noreply.github.com>
2022-10-20 08:57:18 +0800

    Add files via upload

---
+++ 13queue容器.cpp
@@ -0,0 +1,21 @@
+//#include <iostream>
+//using namespace std;
+//#include <string>
+//#include <queue>
+//
+//void test13()
+//{
+//	queue<int>q;
+//	q.push(10);
+//	cout << q.front() << endl;
+//	cout << q.back() << endl;
+//	cout << q.size() << endl;
+//	cout << q.empty() << endl;
+//
+//}
+//int main()
+//{
+//	test13();
+//	system("pause");
+//	return 0;
+//}
